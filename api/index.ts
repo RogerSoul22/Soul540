92c94bcec0641c071687d9418dafd1d5cb81e03c
Filipe <filipesantana859@gmail.com>
2026-03-20 15:24:45 -0300

    debug: expose error message in 500 response

--- api/index.ts
+++ api/index.ts
@@ -5,9 +5,13 @@ import app from '../server/app';
 let dbConnected = false;
 
 export default async function handler(req: any, res: any) {
-  if (!dbConnected) {
-    await connectDB();
-    dbConnected = true;
+  try {
+    if (!dbConnected) {
+      await connectDB();
+      dbConnected = true;
+    }
+    app(req, res);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message, stack: err.stack?.split('\n')[0] });
   }
-  app(req, res);
 }
